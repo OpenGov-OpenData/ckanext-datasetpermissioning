86e1a751ad358eb6487779bc42fcd6ab5b63d2a3
Ian Ward <ian@excess.org>
2016-11-25 21:33:54 -0500

    view users: permission labels implementation

--- ckanext/datasetpermissioning/plugins.py
+++ ckanext/datasetpermissioning/plugins.py
@@ -1,9 +1,23 @@
 from ckanext.datasetpermissioning import helpers
 
-from ckan.plugins import SingletonPlugin, implements, ITemplateHelpers
+from ckan.plugins import (SingletonPlugin, implements, ITemplateHelpers,
+    IPermissionLabels)
+from ckan.lib.plugins import DefaultPermissionLabels
 
-class DatasetPermissioning(SingletonPlugin):
+class DatasetPermissioning(SingletonPlugin, DefaultPermissionLabels):
     implements(ITemplateHelpers)
+    implements(IPermissionLabels)
 
     def get_helpers(self):
         return {'nonsysadmin_user_choices': helpers.nonsysadmin_user_choices}
+
+    def get_dataset_labels(self, dataset_obj):
+        view_users = json.loads(dataset_obj.extras.get('view_users', '[]'))
+        labels = super(BostonSchema, self).get_dataset_labels(dataset_obj)
+        return labels + [u'view-%s' % u for u in view_users]
+
+    def get_user_dataset_labels(self, user_obj):
+        labels = super(BostonSchema, self).get_user_dataset_labels(user_obj)
+        if user_obj:
+            labels.append(u'view-%s' % user_obj.id)
+        return labels
