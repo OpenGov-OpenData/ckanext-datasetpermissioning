2406de31a014f28616730c499d48427dd7dd12cb
Ian Ward <ian@excess.org>
2016-12-02 14:38:13 -0500

    fix some supers

--- ckanext/datasetpermissioning/plugins.py
+++ ckanext/datasetpermissioning/plugins.py
@@ -13,11 +13,11 @@ class DatasetPermissioning(SingletonPlugin, DefaultPermissionLabels):
 
     def get_dataset_labels(self, dataset_obj):
         view_users = json.loads(dataset_obj.extras.get('view_users', '[]'))
-        labels = super(BostonSchema, self).get_dataset_labels(dataset_obj)
+        labels = super(DatasetPermissioning, self).get_dataset_labels(dataset_obj)
         return labels + [u'view-%s' % u for u in view_users]
 
     def get_user_dataset_labels(self, user_obj):
-        labels = super(BostonSchema, self).get_user_dataset_labels(user_obj)
+        labels = super(DatasetPermissioning, self).get_user_dataset_labels(user_obj)
         if user_obj:
             labels.append(u'view-%s' % user_obj.id)
         return labels
