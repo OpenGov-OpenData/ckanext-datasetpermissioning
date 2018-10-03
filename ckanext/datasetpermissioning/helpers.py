8dd1a65dbaaa22002f8ade5285ed0d214d83280b
Jay Guo <jguo144@gmail.com>
2018-10-03 12:01:53 -0400

    Add username to label in case of duplicate display names

--- ckanext/datasetpermissioning/helpers.py
+++ ckanext/datasetpermissioning/helpers.py
@@ -1,10 +1,10 @@
-from ckanapi import LocalCKAN
+import ckan.logic as logic
 
 def nonsysadmin_user_choices(field):
     """
     Return the non-sysadmin users as a choice list
     """
-    lc = LocalCKAN(username='')
+    user_list = logic.get_action('user_list')({},{})
     return [
-        {'value': u['id'], 'label': u['display_name']}
-        for u in lc.action.user_list() if not u['sysadmin']]
+        {'value': u['id'], 'label': u['display_name']+' ('+u['name']+')' }
+        for u in user_list if not u['sysadmin']]
